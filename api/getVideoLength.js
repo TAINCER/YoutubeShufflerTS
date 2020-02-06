eb6ea20ea2dfe5ec2ecc878ebe56a4113708607c
Timm O <taincerhd@hotmail.de>
2020-02-06 12:53:09 +0100

    api changed to complete error

--- api/getVideoLength.js
+++ api/getVideoLength.js
@@ -8,6 +8,6 @@ module.exports = (req, res) => {
         res.status(200).json(response);
       })
       .catch(error => {
-        res.status(500).send(error.message);
+        res.status(500).send(error);
       })
 }
